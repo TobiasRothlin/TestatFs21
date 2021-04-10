93a95a10e75aefcb76c89e07d848fea7ae190c65
TobiasRothlin <tobias@rothlin.com>
2021-04-10 16:09:07 +0200

    Minimum requirements met

--- RPNCalculator.py
+++ RPNCalculator.py
@@ -1,32 +1,236 @@
-import re
+import math
+from enum import Enum
 
+
+# --------------------------
+# Enum class for debugging options
+# --------------------------
+class DebugComponents(Enum):
+    noDebugging = 0  # normal operation
+    stackBuild = 1  # RPNCalculator.execute() returns the current stack
+    variableBuild = 2  # RPNCalculator.execute() returns the current variables
+    functionToExecuteBuild = 3  # RPNCalculator.execute() returns the current functionsToExecute
+
+
+#                                 (should now always be an empty list)
+# --------------------------
+# Main class RPNCalculator
+#
+#
+#
+# public:
+#           method:             RPNCalculator()
+#                               .execute(str, debug = DebugComponents.noDebugging)
+#                                                       ->  executes the string in var str optional param debug
+#
+#           attribute:          .stack[]                ->  contains all numbers and variables <string>
+#                                                           for the current calculation
+#                               .variables{}            ->  contains key :variables <string> | value: variables as value
+#
+#           static method:      none
+#           static attribute:   none
+#
+# private:
+#           method:             __doNextCalculation()   ->  gets the next string from the functionToExecute list
+#                                                           runs the corresponding method deletes the uses values
+#                                                           from the stack and functionToExecuteBuild lists. Then
+#                                                           appends the result to the stack list.
+#                               .__getinputvalues(str)  ->  parses the str<string>, returns the next stack values
+#                                                           and the next functionToExecute list.
+#
+#           attribute:          .__functionToExecute[]  ->  contains all the functions to execute. This list will be
+#                                                           empty as the function returns
+#                               .__functionNames{}      ->  maps the function name <string> to the function to be called
+#                                                           contains key: type of Operation
+#                                                           <string> | value: dictionary with function <dict> ->
+#                                                           key: operation <string> | value: corresponding function <?>
+#
+#           static method:      all calculation methods
+#           static attribute:   .__const{}              ->  contains all constance the class knows
+#                                                           key: constance name <string> | value constance value <float>
+#
+# --------------------------
 
 class RPNCalculator:
+    __const = {
+        'e': 2.718281828159045,
+        'pi': 3.141592653589793,
+    }
+
     def __init__(self):
         self.stack = []
         self.variables = {}
+        self.__functionToExecute = []
+        self.__functionNames = {
+            'dualOperators':
+                {
+                    '+': RPNCalculator.__add,
+                    '-': RPNCalculator.__sub,
+                    '*': RPNCalculator.__multiply,
+                    '/': RPNCalculator.__divide,
+                    '%': RPNCalculator.__modulo,
+                    '==': RPNCalculator.__equal,
+                    '>=': RPNCalculator.__greaterequalthan,
+                    '>': RPNCalculator.__greaterthan,
+                    '<=': RPNCalculator.__lessequalthan,
+                    '<': RPNCalculator.__lessthan,
+                    'pow': math.pow
+                },
+            'singleOperators':
+                {
+                    'abs': RPNCalculator.__abs,
+                    'sign': RPNCalculator.__sign,
+                    'sin': math.sin,
+                    'cos': math.cos,
+                    'tan': math.tan,
+                    'asin': math.asin,
+                    'acos': math.acos,
+                    'atan': math.atan,
+                    'exp': math.exp,
+                    'log': math.log,
+                    'log10': math.log10,
+                    'sqrt': math.sqrt,
+                    'floor': math.floor,
+                    'ceil': math.ceil,
+                    'round': round
 
-    def execute(self, text):
-        self.stack = self.__getinputvalues(text)
+                }
+        }
+
+    def execute(self, str, debug=DebugComponents.noDebugging):
+        newValues = self.__getinputvalues(str)
+        self.stack.extend(newValues[0])
+        self.__functionToExecute.extend(newValues[1])
+        if debug == DebugComponents.stackBuild:
+            return self.stack
+
+        elif debug == DebugComponents.variableBuild:
+            return self.variables
+
+        elif debug == DebugComponents.functionToExecuteBuild:
+            return self.__functionToExecute
+
+        while len(self.__functionToExecute) > 0:
+            self.stack.append(self.__doNextCalculation())
+
         return self.stack
 
-    @staticmethod
-    def __getinputvalues(text):
-        text = text.replace('\n', " ")
-        rawinputlist = text.split(" ")
-        inputlist = []
+    def __doNextCalculation(self):
+        if self.__functionToExecute[0] in self.__functionNames['dualOperators'].keys():
+            if len(self.stack) < 2:
+                raise TypeError(self.__functionToExecute[0] + "got too few arguments")
+            result = self.__functionNames['dualOperators'][self.__functionToExecute[0]](self.stack[-2], self.stack[-1])
+            self.stack.remove(self.stack[-1])
+        elif self.__functionToExecute[0] in self.__functionNames['singleOperators'].keys():
+            if len(self.stack) < 1:
+                raise TypeError(self.__functionToExecute[0] + "got too few arguments")
+            result = self.__functionNames['singleOperators'][self.__functionToExecute[0]](self.stack[-1])
+        else:
+            print("Function Not Found!!!!!!")
+            raise KeyError("Function not found")
+        self.__functionToExecute.remove(self.__functionToExecute[0])
+        self.stack.remove(self.stack[-1])
+        print("Calculating:")
+        print(result)
+        return result
+
+    def __getinputvalues(self, str):
+        str = str.replace('\n', " ")
+        rawinputlist = str.split(" ")
+        stacklist = []
+        funclist = []
+
         for element in rawinputlist:
             if len(element) > 0:
-                inputlist.append(RPNCalculator.__classifystirng(element))
-        return inputlist
+                cleanstring = element.lower()
+                cleanstring = cleanstring.replace(',', '.')
+                if cleanstring.count('.') > 0:
+                    stacklist.append(float(cleanstring))
+                elif cleanstring.isdigit() or (cleanstring.count('-') > 0 and len(cleanstring) > 1):
+                    stacklist.append(int(cleanstring))
+                else:
+                    if cleanstring[0].isdigit():
+                        raise SyntaxError('invalid variable name ' + cleanstring)
+                    elif cleanstring in self.__functionNames['dualOperators'].keys() or cleanstring in \
+                            self.__functionNames['singleOperators'].keys():
+                        funclist.append(cleanstring)
+                    elif cleanstring in RPNCalculator.__const.keys():
+                        stacklist.append(RPNCalculator.__const[cleanstring])
+                    else:
+                        stacklist.append(cleanstring)
+        return [stacklist, funclist]
+
+    # --------------------------
+    # Calculation Functions
+    # --------------------------
 
     @staticmethod
-    def __classifystirng(text):
-        cleanstring = text.lower()
-        cleanstring = cleanstring.replace(',', '.')
-        if cleanstring.count('.') > 0:
-            return float(cleanstring)
-        elif cleanstring.isdigit():
-            return int(cleanstring)
+    def __add(val1, val2):
+        return val1 + val2
+
+    @staticmethod
+    def __sub(val1, val2):
+        return val1 - val2
+
+    @staticmethod
+    def __multiply(val1, val2):
+        return val1 * val2
+
+    @staticmethod
+    def __divide(val1, val2):
+        return val1 / val2
+
+    @staticmethod
+    def __modulo(val1, val2):
+        return val1 % val2
+
+    @staticmethod
+    def __equal(val1, val2):
+        if val1 == val2:
+            return 1
         else:
-            return cleanstring
+            return 0
+
+    @staticmethod
+    def __greaterequalthan(val1, val2):
+        if val1 >= val2:
+            return 1
+        else:
+            return 0
+
+    @staticmethod
+    def __greaterthan(val1, val2):
+        if val1 > val2:
+            return 1
+        else:
+            return 0
+
+    @staticmethod
+    def __lessequalthan(val1, val2):
+        if val1 <= val2:
+            return 1
+        else:
+            return 0
+
+    @staticmethod
+    def __lessthan(val1, val2):
+        if val1 < val2:
+            return 1
+        else:
+            return 0
+
+    @staticmethod
+    def __abs(val1):
+        if val1 < 0:
+            return -1 * val1
+        else:
+            return val1
+
+    @staticmethod
+    def __sign(val1):
+        if val1 < 0:
+            return -1
+        elif val1 > 0:
+            return 1
+        else:
+            return 0
--- TestForRPNCalculator.py
+++ TestForRPNCalculator.py
@@ -1,37 +1,370 @@
 import unittest
 from RPNCalculator import RPNCalculator
+from RPNCalculator import DebugComponents
 
 
 class TestCalculator(unittest.TestCase):
+
+    # --------------------------
+    # Testing functionality stack
+    # --------------------------
+
     def testStack_IntAndVar(self):
         rpn = RPNCalculator()
-        self.assertEqual(rpn.execute("1 2 3 my_var"), [1, 2, 3, "my_var"])
+        print("Result: ", rpn.execute("1 2 3 my_var", DebugComponents.stackBuild))
+        self.assertEqual(rpn.stack, [1, 2, 3, "my_var"])
+
+    def testStack_IntNegNumbers(self):
+        rpn = RPNCalculator()
+        print("Result: ", rpn.execute("-1 -2 -3 my_var", DebugComponents.stackBuild))
+        self.assertEqual(rpn.stack, [-1, -2, -3, "my_var"])
+
+    def testStack_FloatNegNumbers(self):
+        rpn = RPNCalculator()
+        print("Result: ", rpn.execute("-1.5 -2,1 -3.6 my_var", DebugComponents.stackBuild))
+        self.assertEqual(rpn.stack, [-1.5, -2.1, -3.6, "my_var"])
 
     def testStack_ExcessSpaces(self):
         rpn = RPNCalculator()
-        self.assertEqual(rpn.execute("1   2   3   my_var"), [1, 2, 3, "my_var"])
+        print("Result: ", rpn.execute("1   2   3   my_var", DebugComponents.stackBuild))
+        self.assertEqual(rpn.stack, [1, 2, 3, "my_var"])
 
     def testStack_ExcessSpacesAndNewLines(self):
         rpn = RPNCalculator()
-        self.assertEqual(rpn.execute("""1   2  \n 3  \nmy_var"""), [1, 2, 3, "my_var"])
+        print("Result: ", rpn.execute("""1   2  \n 3  \nmy_var""", DebugComponents.stackBuild))
+        self.assertEqual(rpn.stack, [1, 2, 3, "my_var"])
 
     def testStack_IntFloatAndVar(self):
         rpn = RPNCalculator()
-        self.assertEqual(rpn.execute("1 2.0 3 my_var"), [1, 2.0, 3, "my_var"])
+        print("Result: ", rpn.execute("1 2.0 3 my_var", DebugComponents.stackBuild))
+        self.assertEqual(rpn.stack, [1, 2.0, 3, "my_var"])
 
     def testStack_Int(self):
         rpn = RPNCalculator()
-        self.assertEqual(rpn.execute("1000 123412 12412311234123 13 14 13"), [1000, 123412, 12412311234123, 13, 14, 13])
+        print("Result: ", rpn.execute("1000 123412 12412311234123 13 14 13", DebugComponents.stackBuild))
+        self.assertEqual(rpn.stack, [1000, 123412, 12412311234123, 13, 14, 13])
 
     def testStack_Var(self):
         rpn = RPNCalculator()
-        self.assertEqual(rpn.execute("my_var"), ["my_var"])
+        print("Result: ", rpn.execute("my_var", DebugComponents.stackBuild))
+        self.assertEqual(rpn.stack, ["my_var"])
 
     def testStack_FloatWithCommasAndDecPoint(self):
         rpn = RPNCalculator()
-        self.assertEqual(rpn.execute("1,2 2.3 3,1 "), [1.2, 2.3, 3.1])
+        print("Result: ", rpn.execute("1,2 2.3 3,1 ", DebugComponents.stackBuild))
+        self.assertEqual(rpn.stack, [1.2, 2.3, 3.1])
 
+    def testStack_ExcessNewLines(self):
+        rpn = RPNCalculator()
+        print("Result: ", rpn.execute("1\n2\n\n\n4\n", DebugComponents.stackBuild))
+        self.assertEqual(rpn.stack, [1, 2, 4])
 
+    def testStack_InvalidVarName(self):
+        rpn = RPNCalculator()
+        self.assertRaises(SyntaxError, rpn.execute, *{'1 2 3 5my_var': DebugComponents.stackBuild})
+
+    def testStack_constInput(self):
+        rpn = RPNCalculator()
+        print("Result: ", rpn.execute("1 2 5 pi e ", DebugComponents.stackBuild))
+        self.assertEqual(rpn.stack, [1, 2, 5, 3.141592653589793, 2.718281828159045])
+
+    # End stack test
+    # --------------------------
+    # Testing functionality single calculations
+    # --------------------------
+
+    def testFunction_ToExecute_AddFunction(self):
+        rpn = RPNCalculator()
+        res = rpn.execute("1 3.0 +", DebugComponents.functionToExecuteBuild)
+        print("Result: ", res)
+        self.assertEqual(res, ['+'])
+
+    def testFunction_Eval(self):
+        rpn = RPNCalculator()
+        res = rpn.execute("1 2 3 4 +")
+        print("Result: ", res)
+        self.assertEqual(res, [1,2,7])
+
+    def testFunction_Add(self):
+        rpn = RPNCalculator()
+        res = rpn.execute("1 2 3 4 +")
+        print("Result: ", res)
+        self.assertEqual(res, [1, 2, 7])
+
+    def testFunction_Sub(self):
+        rpn = RPNCalculator()
+        res = rpn.execute("1 2 3 4 -")
+        print("Result: ", res)
+        self.assertEqual(res, [1, 2, -1])
+
+    def testFunction_Multiply(self):
+        rpn = RPNCalculator()
+        res = rpn.execute("1 2 3 4 *")
+        print("Result: ", res)
+        self.assertEqual(res, [1, 2, 12])
+
+    def testFunction_Divide(self):
+        rpn = RPNCalculator()
+        res = rpn.execute("1 2 16 4 /")
+        print("Result: ", res)
+        self.assertEqual(res, [1, 2, 4])
+
+    def testFunction_Modulo(self):
+        rpn = RPNCalculator()
+        res = rpn.execute("1 2 15 4 %")
+        print("Result: ", res)
+        self.assertEqual(res, [1, 2, 3])
+
+    def testFunction_Equal(self):
+        rpn = RPNCalculator()
+        res = rpn.execute("1 2 15 4 ==")
+        print("Result: ", res)
+        self.assertEqual(res, [1, 2, 0])
+
+    def testFunction_NotEqual(self):
+        rpn = RPNCalculator()
+        res = rpn.execute("1 2 4 4 ==")
+        print("Result: ", res)
+        self.assertEqual(res, [1, 2, 1])
+
+    def testFunction_GreaterEqualThan(self):
+        rpn = RPNCalculator()
+        res = rpn.execute("1 2 15 4 >=")
+        print("Result: ", res)
+        self.assertEqual(res, [1, 2, 1])
+
+    def testFunction_NotGreaterEqualThan(self):
+        rpn = RPNCalculator()
+        res = rpn.execute("1 2 4 14 >=")
+        print("Result: ", res)
+        self.assertEqual(res, [1, 2, 0])
+
+    def testFunction_NotGreaterThan(self):
+        rpn = RPNCalculator()
+        res = rpn.execute("1 2 4 14 >")
+        print("Result: ", res)
+        self.assertEqual(res, [1, 2, 0])
+
+    def testFunction_GreaterThan(self):
+        rpn = RPNCalculator()
+        res = rpn.execute("1 2 14 4 >")
+        print("Result: ", res)
+        self.assertEqual(res, [1, 2, 1])
+
+    def testFunction_NotGreaterThanWithEqualInput(self):
+        rpn = RPNCalculator()
+        res = rpn.execute("1 2 14 14 >")
+        print("Result: ", res)
+        self.assertEqual(res, [1, 2, 0])
+
+    def testFunction_LessEqualThan(self):
+        rpn = RPNCalculator()
+        res = rpn.execute("1 2 4 14 <=")
+        print("Result: ", res)
+        self.assertEqual(res, [1, 2, 1])
+
+    def testFunction_NotLessEqualThan(self):
+        rpn = RPNCalculator()
+        res = rpn.execute("1 2 14 4 <=")
+        print("Result: ", res)
+        self.assertEqual(res, [1, 2, 0])
+
+    def testFunction_LessThan(self):
+        rpn = RPNCalculator()
+        res = rpn.execute("1 2 4 14 <")
+        print("Result: ", res)
+        self.assertEqual(res, [1, 2, 1])
+
+    def testFunction_NotLessThan(self):
+        rpn = RPNCalculator()
+        res = rpn.execute("1 2 14 4 <")
+        print("Result: ", res)
+        self.assertEqual(res, [1, 2, 0])
+
+    def testFunction_AbsNeg(self):
+        rpn = RPNCalculator()
+        res = rpn.execute("-3 abs")
+        print("Result: ", res)
+        self.assertEqual(res, [3])
+
+    def testFunction_AbsPos(self):
+        rpn = RPNCalculator()
+        res = rpn.execute("3 abs")
+        print("Result: ", res)
+        self.assertEqual(res, [3])
+
+    def testFunction_SignPos(self):
+        rpn = RPNCalculator()
+        res = rpn.execute("3 sign")
+        print("Result: ", res)
+        self.assertEqual(res, [1])
+
+    def testFunction_SignZero(self):
+        rpn = RPNCalculator()
+        res = rpn.execute("0 sign")
+        print("Result: ", res)
+        self.assertEqual(res, [0])
+
+    def testFunction_SignNeg(self):
+        rpn = RPNCalculator()
+        res = rpn.execute("-3 sign")
+        print("Result: ", res)
+        self.assertEqual(res, [-1])
+
+    def testFunction_Sin(self):
+        rpn = RPNCalculator()
+        res = rpn.execute("90 sin")
+        print("Result: ", res)
+        self.assertEqual(res, [0.8939966636005579])
+
+    def testFunction_Cos(self):
+        rpn = RPNCalculator()
+        res = rpn.execute("0 cos")
+        print("Result: ", res)
+        self.assertEqual(res, [1])
+
+    def testFunction_Tan(self):
+        rpn = RPNCalculator()
+        res = rpn.execute("0 tan")
+        print("Result: ", res)
+        self.assertEqual(res, [0])
+
+    def testFunction_Asin(self):
+        rpn = RPNCalculator()
+        res = rpn.execute("1 asin")
+        print("Result: ", res)
+        self.assertEqual(res, [1.5707963267948966])
+
+    def testFunction_Acos(self):
+        rpn = RPNCalculator()
+        res = rpn.execute("0 acos")
+        print("Result: ", res)
+        self.assertEqual(res, [1.5707963267948966])
+
+    def testFunction_Atan(self):
+        rpn = RPNCalculator()
+        res = rpn.execute("0 atan")
+        print("Result: ", res)
+        self.assertEqual(res, [0])
+
+    def testFunction_Exp(self):
+        rpn = RPNCalculator()
+        res = rpn.execute("2 exp")
+        print("Result: ", res)
+        self.assertEqual(res, [7.38905609893065])
+
+    def testFunction_Log(self):
+        rpn = RPNCalculator()
+        res = rpn.execute("1 log")
+        print("Result: ", res)
+        self.assertEqual(res, [0])
+
+    def testFunction_Log10(self):
+        rpn = RPNCalculator()
+        res = rpn.execute("10000 log10")
+        print("Result: ", res)
+        self.assertEqual(res, [4])
+
+    def testFunction_pow(self):
+        rpn = RPNCalculator()
+        res = rpn.execute("5 7 pow")
+        print("Result: ", res)
+        self.assertEqual(res, [78125])
+
+    def testFunction_Sqrt(self):
+        rpn = RPNCalculator()
+        res = rpn.execute("25 sqrt")
+        print("Result: ", res)
+        self.assertEqual(res, [5])
+
+    def testFunction_Floor(self):
+        rpn = RPNCalculator()
+        res = rpn.execute("5.999 floor")
+        print("Result: ", res)
+        self.assertEqual(res, [5])
+
+    def testFunction_Ceil(self):
+        rpn = RPNCalculator()
+        res = rpn.execute("4.005 ceil")
+        print("Result: ", res)
+        self.assertEqual(res, [5])
+
+    def testFunction_Round(self):
+        rpn = RPNCalculator()
+        res = rpn.execute("5.3 round")
+        print("Result: ", res)
+        self.assertEqual(res, [5])
+
+    # End single calculations test
+    # --------------------------
+    # Testing functionality multiple calculations
+    # --------------------------
+
+    def testMultipleCalculations_1(self):
+        rpn = RPNCalculator()
+        res = rpn.execute("3 4 5 + +")
+        print("Result: ", res)
+        self.assertEqual(res, [12])
+
+    def testMultipleCalculations_2(self):
+        rpn = RPNCalculator()
+        res = rpn.execute("5 3 4 + *")
+        print("Result: ", res)
+        self.assertEqual(res, [35])
+
+    def testMultipleCalculations_3(self):
+        rpn = RPNCalculator()
+        res = rpn.execute("5 4 3 - *")
+        print("Result: ", res)
+        self.assertEqual(res, [5])
+
+    def testMultipleCalculations_4(self):
+        rpn = RPNCalculator()
+        res = rpn.execute("5 6 14 + /")
+        print("Result: ", res)
+        self.assertEqual(res, [0.25])
+
+    def testMultipleCalculations_5(self):
+        rpn = RPNCalculator()
+        rpn.execute("6 14 +")
+        res = rpn.execute("5 /")
+        print("Result: ", res)
+        self.assertEqual(res, [4])
+
+    def testMultipleCalculations_6(self):
+        rpn = RPNCalculator()
+        rpn.execute("6 14 +")
+        res = rpn.execute("13 / round")
+        print("Result: ", res)
+        self.assertEqual(res, [2])
+
+    def testMultipleCalculations_7(self):
+        rpn = RPNCalculator()
+        rpn.execute("6")
+        rpn.execute("14")
+        rpn.execute("+")
+        rpn.execute("13")
+        rpn.execute("/")
+        res = rpn.execute("round")
+        print("Result: ", res)
+        self.assertEqual(res, [2])
+
+    # End multiple calculations test
+    # --------------------------
+    # Testing functionality exception handling
+    # --------------------------
+
+    def testException_InvalidVarName(self):
+        rpn = RPNCalculator()
+        self.assertRaises(SyntaxError, rpn.execute, *{'1 2 3 5my_var': DebugComponents.stackBuild})
+
+    def testException_TooFewArgumentsDualOperations(self):
+        rpn = RPNCalculator()
+        self.assertRaises(TypeError, rpn.execute, *{'2 pow'})
+
+    def testException_TooFewArgumentsSingle(self):
+        rpn = RPNCalculator()
+        self.assertRaises(TypeError, rpn.execute, *{'abs'})
 
 
 if __name__ == '__main__':
